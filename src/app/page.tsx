e3a01109c135eceab23b4f23d78b92180feef05a
AzracStudios <azracstudios@gmail.com>
2024-12-22 13:20:49 +0530

    impl: banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
+import Landing from "@/section/landing/landing";
+
 export default function Home() {
   return (
-    <h1>Hello World!</h1>
+    <div className="page">
+      <Landing />     
+    </div>
   );
 }
