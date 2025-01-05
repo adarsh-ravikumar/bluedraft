d6885304b363b66e3ed1693743f1ea560bcc960c
vasanth2896 <vnayak2896@gmail.com>
2025-01-05 19:57:16 +0530

    Vasanth | Completed whyUs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import AboutUs from "@/sections/about_us/AboutUs";
 import Footer from "@/sections/footer/footer";
 import Landing from "@/sections/landing/landing";
 import WhatWeDo from "@/sections/what_we_do/wwd";
+import WhyUs from "@/sections/why_us/WhyUs";
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
       <Landing />
       <AboutUs/>
       <WhatWeDo />
+      <WhyUs/>
       <Footer />
     </div>
   );
