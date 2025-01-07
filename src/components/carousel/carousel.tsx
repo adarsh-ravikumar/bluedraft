bfc6a17b8d453d9c46ea3c45a2253b2878ca7631
adarsh_ravikumar <azracstudios@gmail.com>
2025-01-07 19:28:27 +0530

    delete: unwanted files with error

--- src/components/carousel/carousel.tsx
+++
@@ -1,7 +0,0 @@
-export default function Carousel({images}: {images:string[]}){
-    return (
-        <div className="projects__carousel">
-            <h1>Carousel</h1>
-        </div>
-    )
-}
--- src/sections/projects/projects.tsx
+++
@@ -1,9 +0,0 @@
-import Carousel from "@/components/carousel/carousel";
-
-export default function Projects() {
-    return (
-        <div className="projects">
-            <Carousel images={[]}/>
-        </div>
-    );
-}
