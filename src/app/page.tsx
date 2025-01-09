1015d7fb628cd3e297f068e9da11516d88ce23d2
adarsh_ravikumar <azracstudios@gmail.com>
2025-01-09 07:36:24 +0530

    impl: projects

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,16 @@
-import Footer from "@/sections/footer/footer";
-import Landing from "@/sections/landing/landing";
-import WhatWeDo from "@/sections/what_we_do/wwd";
+// import Footer from "@/sections/footer/footer";
+// import Landing from "@/sections/landing/landing";
+// import WhatWeDo from "@/sections/what_we_do/wwd";
+
+import Projects from "@/sections/projects/projects";
 
 export default function Home() {
   return (
     <div className="page">
-      <Landing />
+      {/* <Landing />
       <WhatWeDo />
-      <Footer />
+      <Footer /> */}
+      <Projects />
     </div>
   );
 }
